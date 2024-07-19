954a37ae520280e769e57b1d016eceef613223ff
Debi7 <debi1@mail.ru>
2024-07-19 05:27:26 +0300

    Updates

--- src/apiConfig.ts
+++
@@ -1,7 +0,0 @@
-export const BASE_URL: string = 'https://sandbox.creos.me/api/v1';
-export const API_ENDPOINTS = {
-  comments: `${BASE_URL}/comment`,
-  designers: `${BASE_URL}/designer`,
-  issues: `${BASE_URL}/issue`,
-  projects: `${BASE_URL}/project`,
-};
